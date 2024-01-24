17c449de6a7226baab1290bb07aeaaa6be9a3d5d
Ali Alshehri <Ali0fawzish@outlook.com>
2024-01-24 22:02:21 +0300

    Update Google callback URL

--- server/server.js
+++ server/server.js
@@ -170,7 +170,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "/auth/google/callback",
+      callbackURL: "https://www.mindboard.live/auth/google/callback",
     },
     async (accessToken, refreshToken, profile, done) => {
       try {
