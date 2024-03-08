b1527a14d9ffa49925c48349a92f15bdd47d3f8a
Ali Alshehri <Ali0fawzish@outlook.com>
2024-03-08 12:46:59 +0300

    Update link

--- server/server.js
+++ server/server.js
@@ -242,7 +242,9 @@ app.get(
 );
 
 // * Get Routes
-app.get("/", passport.authenticate("local"), async (req, res) => {});
+app.get("/", passport.authenticate("local"), async (req, res) => {
+  res.redirect("https://www.mindboard.live/");
+});
 
 app.get("/api/logout", (req, res) => {
   req.logout((err) => {
@@ -470,7 +472,7 @@ app.post("/api/updateUser", async (req, res) => {
 
 async function SendEmail(email, authCode) {
   const resend = new Resend(process.env.RESEND_API);
-  const verificationLink = `https://mindboard.live/api/verifyAccount?email=${email}&token=${authCode}`;
+  const verificationLink = `https://www.mindboard.live/api/verifyAccount?email=${email}&token=${authCode}`;
 
   await resend.emails.send({
     from: "MindBoard <Verify@mindboard.live>",
