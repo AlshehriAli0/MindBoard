2e8ec42b4c1d83b73da9aef3445cd6a1cb3fa58d
Ali Alshehri <Ali0fawzish@outlook.com>
2024-03-08 12:33:39 +0300

    Update verification link to use HTTPS

--- server/server.js
+++ server/server.js
@@ -471,7 +471,7 @@ app.post("/api/updateUser", async (req, res) => {
 
 async function SendEmail(email, authCode) {
   const resend = new Resend(process.env.RESEND_API);
-  const verificationLink = `http://mindboard.live/api/verifyAccount?email=${email}&token=${authCode}`;
+  const verificationLink = `https://mindboard.live/api/verifyAccount?email=${email}&token=${authCode}`;
 
   await resend.emails.send({
     from: "MindBoard <Verify@mindboard.live>",
